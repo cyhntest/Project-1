bce127adb6da8195299959015660a982db7f9045
cyhntest <41551707+cyhntest@users.noreply.github.com>
2018-07-24 02:08:13 +0400

    V2.0.1

--- script/main.js
+++ script/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
-    
+
+    // FOOD IMAGE SIZES
+    var foodImageWidth = $('.foodimage').width();
+    $('.foodimage').css('height', foodImageWidth);
+
+    // ACCORDION
+    $('.collapse').on('shown.bs.collapse', function(){
+        $(this).parent().find(".card-header").addClass("activeCard")
+    })
+    $('.collapse').on('hidden.bs.collapse', function(){
+        $(this).parent().find(".activeCard").removeClass("activeCard")
+    })
+
+
+
+    // COPYRIGHT YEAR
+    var year = (new Date()).getFullYear()
+    $(".copyright span").html(year)
 });
 
 window.addEventListener("load", function () {
